cf95889aef0bbcbfa5a51d9f864005f97f48a68c
Petr Koval <evilsephy1@mail.ru>
2025-04-25 09:51:21 +0300

    controls triggers component's attributes changing

--- script/index.js
+++ script/index.js
@@ -1,3 +1,31 @@
 import ProgressLoader from "./components/progress-loader.js";
 
 customElements.define('progress-loader', ProgressLoader);
+
+const loader = document.querySelector('progress-loader');
+
+const valueInput = document.querySelector('input#value');
+const animateInput = document.querySelector('input#animate');
+const hideInput = document.querySelector('input#hide');
+
+valueInput.addEventListener('change', e => {
+    if (valueInput.checkValidity() === true) {
+        loader.setAttribute('value', e.target.value);
+    }
+});
+
+animateInput.addEventListener('change', e => {
+    if (e.target.checked) {
+        loader.setAttribute('animate', e.target.checked);
+    } else {
+        loader.removeAttribute('animate');
+    }
+});
+
+hideInput.addEventListener('change', e => {
+    if (e.target.checked) {
+        loader.setAttribute('hidden', e.target.checked);
+    } else {
+        loader.removeAttribute('hidden');
+    }
+});
